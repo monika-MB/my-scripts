c1349980b213d7f093d3456ffb21740a662a49a2
monika-MB <110933585+monika-MB@users.noreply.github.com>
2022-08-09 23:00:45 +0200

    Delete zaladuj.R

--- zaladuj.R
+++
@@ -1,6 +0,0 @@
-zaladuj<<-function(paczka){
-  if(!(suppressWarnings(suppressMessages(require(paczka,character.only = TRUE))))){
-    install.packages(as.character(paczka))
-    suppressWarnings(suppressMessages(library(paczka,character.only = TRUE)))
-  } 
-}
